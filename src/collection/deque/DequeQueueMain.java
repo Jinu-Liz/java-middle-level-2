d0b9d6d28adffdadefc03e5246a2d95eb1c7e1b7
Hyungseock-Yun <bagzein@gmail.com>
2024-09-04 21:39:35 +0900

    - Deque와 Stack, Queue

---
+++ src/collection/deque/DequeQueueMain.java
@@ -0,0 +1,30 @@
+package collection.deque;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Queue;
+
+/**
+ * Queue의 기능 제공
+ */
+public class DequeQueueMain {
+  public static void main(String[] args) {
+    Deque<Integer> deque = new ArrayDeque<>();
+//    Queue<Integer> deque = new ArrayDeque<>();
+
+    // 데이터 추가
+    deque.offer(1);
+    deque.offer(2);
+    deque.offer(3);
+    System.out.println(deque);
+
+    // 다음 꺼낼 데이터 확인(조회만)
+    System.out.println("deque.peek() = " + deque.peek());
+
+    // 데이터 꺼내기
+    System.out.println("deque.pop() = " + deque.poll());
+    System.out.println("deque.pop() = " + deque.poll());
+    System.out.println("deque.pop() = " + deque.poll());
+    System.out.println(deque);
+  }
+}
