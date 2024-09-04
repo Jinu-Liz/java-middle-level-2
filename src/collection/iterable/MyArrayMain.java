c5536cf0f882eb0e6748957d1be1b469d084c125
Hyungseock-Yun <bagzein@gmail.com>
2024-09-05 00:39:27 +0900

    - 순회2 : 향상된 for문

--- src/collection/iterable/MyArrayMain.java
+++ src/collection/iterable/MyArrayMain.java
@@ -12,5 +12,21 @@ public class MyArrayMain {
       Integer value = iterator.next();
       System.out.println("value = " + value);
     }}
+
+    // 추가
+
+    /**
+     * java: for-each not applicable to expression type
+     *   required: array or java.lang.Iterable
+     *   found:    collection.iterable.MyArray
+     *
+     *   Iterable을 구현하지 않으면 해당 에러가 발생.
+     *   향상된 for문은 Iterable 인터페이스를 구현한 객체에 대해
+     *   반복을 사용할 수 있도록 해주는 것을 알 수 있다.
+     */
+    System.out.println("for-each 사용");
+    for (int value : myArray) {
+      System.out.println("value = " + value);
+    }
   }
 }
